c4740b1d05b45a88af516ec33d6084da4bc51557
Alyson Takai <shtakai@gmail.com>
2018-11-04 20:49:51 +0700

    Add /pages route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  get 'pages', to: 'pages#index'
+  root to: "pages#index"
 end
