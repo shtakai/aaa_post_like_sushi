8d498bd39ef4316472e8235d388345476c134250
Alyson Takai <shtakai@gmail.com>
2018-11-06 21:13:11 +0700

    Modify for devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
 
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = 'post okay'
+      redirect_to posts_path
+    else
+      render new
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
