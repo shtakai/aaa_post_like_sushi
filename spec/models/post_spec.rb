e880df7259ac0fdcec968a11ecd28849eef0e0cb
Alyson Takai <shtakai@gmail.com>
2018-11-11 11:30:30 +0700

    Add numeric validation on post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Post do
   it { should belong_to(:user) }
+  it { should validate_length_of(:title).
+       is_at_least(5).
+       is_at_most(100)
+  }
+  it { should validate_length_of(:content).
+       is_at_least(5).
+       is_at_most(1000)
+  }
 
   context 'attributes' do
     subject { build :post }
