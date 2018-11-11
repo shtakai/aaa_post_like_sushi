a7b8d300a2330bac86a81ebb0ea20808d8e94766
Alyson Takai <shtakai@gmail.com>
2018-11-11 10:10:25 +0700

    Add scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
+  LIMIT_AMOUNT = 10
+
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :content, presence: true, length: { minimum: 5, maximum: 1000 }
+
+  scope :newest, -> (n = LIMIT_AMOUNT) { includes(:user).order(updated_at: :desc).limit(n) }
 end
